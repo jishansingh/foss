9e8dc7de7d5df9c44faed3586dc1304d88f7e063
jishansingh <singhjishan2@gmail.com>
2019-05-24 02:16:06 +0545

    add models to admin

--- foss/doubt/admin.py
+++ foss/doubt/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Question,Reply,UserProfile
 # Register your models here.
+admin.site.register(Question)
+admin.site.register(Reply)
+admin.site.register(UserProfile)
+
+
