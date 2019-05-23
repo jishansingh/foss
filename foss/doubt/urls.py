8a5f7ccd50cbf21ad217908f913ad4caafd1423d
jishansingh <singhjishan2@gmail.com>
2019-05-24 00:02:57 +0545

    add virtual env urls

--- foss/doubt/urls.py
+++ foss/doubt/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path,include
 from . import views
-
+from django.contrib.auth.views import login,logout
 urlpatterns = [
     path('',views.index,name='index'),
+    path('login/',login,{'template_name':'login/login.html'},name='login'),
+    path('logout/',views.log_me_out,name='logout'),
+    path('my-ques/',views.my_question,name='my_question'),
+    path('question/<id>',views.Question.as_view(),name='question'),
+    path('all/',views.view_question,name='view_question'),
+    path('register/',views.Register.as_view(),name='register'),
+    path('profile/',views.ViewProfile.as_view(),name='view_profile'),
+    path('edit/',views.EditProfile.as_view(),name='edit_profile'),
 ]
